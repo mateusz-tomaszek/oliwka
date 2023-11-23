6504967746e3e84a1258ef1d753237bfeda6d781
Mateusz <127236338+mateusz-tomaszek@users.noreply.github.com>
2023-11-23 17:26:04 +0100

    Add files via upload

---
+++ files/java.js
@@ -0,0 +1,10 @@
+function year(){
+    var rok = new Date().getFullYear();
+
+    document.write(rok);
+}
+
+
+function dmMail() {
+    window.location.href = 'https://mateusztomaszek.com.pl/form';
+}
