359a122c161cd3c57894fb530b0894b05379aa9c
Mateusz <127236338+mateusz-tomaszek@users.noreply.github.com>
2024-02-05 20:28:49 +0100

    Add files via upload

--- other/script.js
+++ other/script.js
@@ -22,3 +22,38 @@ $(document).ready(function(){
       } // End if
     });
   });
+
+
+  function sprawdzHaslo() {
+    var haslo = prompt("Wprowadź hasło:");
+  
+    if (haslo === "130823") {
+        wyswietlZawartosc();
+    } else {
+        alert("Błędne hasło. Spróbuj ponownie.");
+        sprawdzHaslo();
+    }
+  }
+
+
+
+  
+  function wyswietlZawartosc() {
+    document.getElementById("haslo").style.display = "block";
+    hideFullscreen();
+  }
+
+  function showFullscreen(imageSrc) {
+    var fullscreenContainer = document.getElementById('fullscreenContainer');
+    var fullscreenImage = document.getElementById('fullscreenImage');
+    
+    fullscreenImage.src = imageSrc;
+    fullscreenContainer.style.display = 'block';
+  }
+  
+  function hideFullscreen() {
+    var fullscreenContainer = document.getElementById('fullscreenContainer');
+    
+    fullscreenContainer.style.display = 'none';
+  }
+
